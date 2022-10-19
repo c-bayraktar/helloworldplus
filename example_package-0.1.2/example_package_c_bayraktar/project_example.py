91a3f069a45668db28f486e0860545fe1c9633f4
Ceren Bayraktar <115998130+c-bayraktar@users.noreply.github.com>
2022-10-19 12:39:30 +0200

    Add files via upload

---
+++ example_package-0.1.2/example_package_c_bayraktar/project_example.py
@@ -0,0 +1,9 @@
+def target(obj):
+  if obj == 'exoplanets':
+    print('Hello World!')
+  elif obj == 'exomoons':
+    print('Hello Moon!')
+  elif obj == 'solar':
+    print('Hello Star!')
+  else:
+    print('There was either a typo or this research interest is not included...yet!')
