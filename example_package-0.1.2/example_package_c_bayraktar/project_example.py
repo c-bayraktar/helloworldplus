0f2da727823e79e115116e2c6f3e9c19c74ce2a3
Ceren Bayraktar <115998130+c-bayraktar@users.noreply.github.com>
2022-10-19 12:41:06 +0200

    Delete example_package-0.1.2 directory

--- example_package-0.1.2/example_package_c_bayraktar/project_example.py
+++
@@ -1,9 +0,0 @@
-def target(obj):
-  if obj == 'exoplanets':
-    print('Hello World!')
-  elif obj == 'exomoons':
-    print('Hello Moon!')
-  elif obj == 'solar':
-    print('Hello Star!')
-  else:
-    print('There was either a typo or this research interest is not included...yet!')
